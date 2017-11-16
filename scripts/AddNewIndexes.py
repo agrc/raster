e184687be59c57d07541bd44693e3f1ad0029888
Scott Davis <stdavis@utah.gov>
2017-11-16 16:44:12 -0700

    Automate a large portion of adding new indexes
    Create AddNewIndexes
    Move layer list for pallet into layers.json to ease updating via the new script.

---
+++ scripts/AddNewIndexes.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+AddNewIndexes.py
+
+Used to add new index layers to this app.
+
+Intended to be run on test server (.58) via ArcGIS Desktop python.
+'''
+
+import json
+from os.path import dirname, join, realpath
+from sys import argv
+
+import arcpy
+from raster_secrets import PROD_SERVERS
+
+current_folder = dirname(realpath(__file__))
+local_folder = r'C:\MapData'
+sgid = join(current_folder, 'SGID10 as INDICES.sde')
+
+
+def add_layers_to_pallet(new_layers):
+    print('adding layers to layers.json file (used by RasterPallet.py)')
+    json_path = join(current_folder, 'layers.json')
+    with open(json_path, 'r') as read_file:
+        existing_layers = json.loads(read_file.read())
+
+    with open(json_path, 'w') as write_file:
+        write_file.write(json.dumps(existing_layers + new_layers, indent=4))
+
+
+def add_layers_to_gdbs(new_layers):
+    folders = [local_folder] + PROD_SERVERS.map(lambda ip: r'\\{}\c$\MapData'.format(ip))
+    # folders = [local_folder]
+    print('adding layers to raster.gdb in these folders: {}'.format(folders))
+    for layer in new_layers:
+        for folder in folders:
+            gdb = join(folder, 'raster.gdb')
+            print('projecting {} into {}'.format(layer, gdb))
+            arcpy.management.Project(join(sgid, layer), join(gdb, layer), arcpy.SpatialReference(3857), 'NAD_1983_To_WGS_1984_5')
+
+
+def add_layers_to_mxd(new_layers):
+    print('adding layers to Raster.mxd')
+    mxd_path = join(current_folder, '..', 'maps', 'Raster.mxd')
+    mxd = arcpy.mapping.MapDocument(mxd_path)
+    template_path = join(current_folder, 'template.lyr')
+
+    for layer_name in new_layers:
+        print(layer_name)
+        layer = arcpy.mapping.Layer(template_path)
+        layer.name = layer_name.split('.')[-1]
+        layer.replaceDataSource(join(local_folder, 'raster.gdb'), 'FILEGDB_WORKSPACE', layer_name.split('.')[-1])
+        arcpy.mapping.AddLayer(mxd.activeDataFrame, layer, 'BOTTOM')
+
+    mxd.save()
+
+    lyrs = arcpy.mapping.ListLayers(mxd)
+
+    def FindLayer(name):
+        for l in lyrs:
+            if l.name == name:
+                return True
+
+        print('ERROR! Could not find layer: %s' % name)
+
+    print('validating that all indexes in the extents feature classes (SDE) are added as layers in mxd')
+    for i in range(4):
+        cur = arcpy.SearchCursor(join(sgid, lyrs[i].name), "In_House = 'Yes' AND SHOW = 'Y'")
+        print('searching %s' % lyrs[i])
+        for row in cur:
+            FindLayer(row.Tile_Index.split('.')[2])
+
+
+try:
+    new_layers = argv[1].split(';')
+except:
+    new_layers = raw_input('New layer(s) separated by ";": ').split(';')
+
+add_layers_to_pallet(new_layers)
+add_layers_to_gdbs(new_layers)
+add_layers_to_mxd(new_layers)
+
+print('SUCCESS!!!')
+print('Next step: Open Raster.mxd and publish to prod')
+print('Note: The extents feature class was not updated during this process.')
--- scripts/RasterPallet.py
+++ scripts/RasterPallet.py
@@ -8,11 +8,13 @@ It updates all data in the raster.gdb database.
 The DRG.gdb database is managed manually in staging.
 '''
 
+from json import loads
 from os.path import dirname, join, realpath
 
 from forklift.models import Pallet
 
 current_folder = dirname(realpath(__file__))
+layersJSONFile = join(current_folder, 'layers.json')
 
 
 class RasterPallet(Pallet):
@@ -24,79 +26,7 @@ class RasterPallet(Pallet):
         self.copy_data = [self.raster]
         self.arcgis_services = [('Raster', 'MapServer')]
 
-        self.add_crates(['Aerial_Photography_Extents',
-                         'AutoCorrelated_DEM_2m',
-                         'AutoCorrelated_DEM_5m',
-                         'BLM_1970s_QQuads',
-                         'Contour_Line_Extents',
-                         'Contours_2meter_SLCo',
-                         'DEM_Extents',
-                         'DEM10_Quads',
-                         'DEM30_Quads',
-                         'DEM30_Statewide',
-                         'DEM90_Quads',
-                         'DEM90_Statewide',
-                         'DOQ_County',
-                         'DOQ_Quads',
-                         'HRO2006',
-                         'HRO2009',
-                         'HRO2012_4_Band',
-                         'HRO2012_UintahBasin',
-                         'HRO2012',
-                         'Iron_Beaver2003_Color',
-                         'LiDAR_Extents',
-                         'LiDAR2006_1_25m_BareEarth_Tiles',
-                         'LiDAR2006_1_25m_Raw_Tiles',
-                         'LiDAR2006_1m_BareEarth_Tiles',
-                         'LiDAR2006_1m_FirstReturn_Tiles',
-                         'LiDAR2006_1m_LastReturn_Tiles',
-                         'LiDAR2006_1m_Raw_Tiles',
-                         'LiDAR2006_2m_BareEarth_Tiles',
-                         'LiDAR2006_2m_Raw_Tiles',
-                         'LiDAR2011_10ft_DavisCo_DEM_Tile',
-                         'LiDAR2011_1m_BearRiver_DEM_Tiles',
-                         'LiDAR2011_1m_CedarValley_DEM_Tiles',
-                         'LiDAR2011_1m_GSL_Middle_DEM_Tiles',
-                         'LiDAR2011_1m_GSL_North_DEM_Tiles',
-                         'LiDAR2011_1m_GSL_South_DEM_Tiles',
-                         'LiDAR2011_1m_GSL_Tooele_DEM_Tiles',
-                         'LiDAR2011_1m_HurricaneFault_DEM_Tiles',
-                         'LiDAR2011_1m_LowryWater_DEM_Tiles',
-                         'LiDAR2011_1m_OgdenFEMA_DEM_Tiles',
-                         'LiDAR2011_1m_OgdenValley_DEM_Tiles',
-                         'LiDAR2013_1m_SanRafael_Tile',
-                         'LiDAR2013_2014_50cm_WasatchFront_DSM_Tiles',
-                         'LiDAR2013_2014_50cm_WasatchFront_DTM_Tiles',
-                         'LiDAR2015_50cm_Rivers_DEM_Tiles',
-                         'LiDAR2015_50cm_Rivers_DSM_Tiles',
-                         'LiDAR2016_50cm_MonroeMtn_DEM_Tiles',
-                         'LiDAR2016_50cm_MonroeMtn_DSM_Tiles',
-                         'LiDAR2017_50cm_WVW_DEM_Tiles',
-                         'LiDAR2017_50cm_WVW_DSM_Tiles',
-                         'NAIP2003_County',
-                         'NAIP2003_QQuads',
-                         'NAIP2004_County',
-                         'NAIP2004_QQuads',
-                         'NAIP2006_CIR_County',
-                         'NAIP2006_CIR_QQuads',
-                         'NAIP2006_County',
-                         'NAIP2006_QQuads',
-                         'NAIP2009_4_Band_QQuads',
-                         'NAIP2009_County',
-                         'NAIP2009_QQuads',
-                         'NAIP2011_4_Band_QQuads',
-                         'NAIP2011_County',
-                         'NAIP2014_Band4_QQuads',
-                         'NAIP2014_County',
-                         'NAIP2014_RGB_QQuads',
-                         'NAIP2016_Band4_QQuads',
-                         'NAIP2016_County',
-                         'NAIP2016_RGB_QQuads',
-                         'NED10_County',
-                         'NED10_Quads',
-                         'NED30_County',
-                         'NED30_Statewide',
-                         'SLCo_1977_County',
-                         'SLCo_1977_Quads',
-                         'UAO2003'],
-                        {'source_workspace': self.sgid, 'destination_workspace': self.raster})
+        with open(layersJSONFile) as jfile:
+            layers = loads(jfile.read())
+
+        self.add_crates(layers, {'source_workspace': self.sgid, 'destination_workspace': self.raster})
