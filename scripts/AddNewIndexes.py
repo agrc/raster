181f33d448950eb5427c4078de27c4e239eeb540
Scott Davis <stdavis@utah.gov>
2019-01-18 16:12:30 -0700

    fix: restore layers.json to prevent forklift from hashing more data that needed

--- scripts/AddNewIndexes.py
+++ scripts/AddNewIndexes.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python
-# * coding: utf8 *
 '''
 AddNewIndexes.py
 
 Used to add new index layers to this app.
 
-Intended to be run on test server (.58) via ArcGIS Desktop python.
+Intended to be run on local server via ArcGIS Desktop python instance.
 '''
 
 import json
-from os.path import dirname, join, realpath
+from os.path import dirname, join, realpath, basename
 from sys import argv
 
 import arcpy
-from raster_secrets import PROD_SERVERS
+from raster_secrets import PROD_SERVERS, SHARE
 
 current_folder = dirname(realpath(__file__))
-local_folder = r'C:\MapData'
+local_folder = r'C:\forklift\data\production'
 sgid = join(current_folder, 'SGID10 as INDICES.sde')
+
+
+def add_layers_to_pallet(new_layers):
+    print('adding layers to layers.json file (used by RasterPallet.py)')
+    json_path = join(current_folder, 'layers.json')
+    with open(json_path, 'r') as read_file:
+        existing_layers = json.loads(read_file.read())
+
+    with open(json_path, 'w') as write_file:
+        write_file.write(json.dumps(existing_layers + map(lambda l: basename(l), new_layers), indent=4))
 
 
 def add_layers_to_gdbs(new_layers):
     folders = [local_folder] + map(lambda ip: r'\\{}\c$\MapData'.format(ip), PROD_SERVERS)
     # folders = [local_folder]
-    print('adding layers to raster.gdb in these folders: {}'.format(folders))
+    print('adding layers to raster gdbs in these folders: {}'.format(folders))
     for layer in new_layers:
         for folder in folders:
-            gdb = join(folder, 'raster.gdb')
-            print('projecting {} into {}'.format(layer, gdb))
-            arcpy.management.Project(join(sgid, layer), join(gdb, layer), arcpy.SpatialReference(3857), 'NAD_1983_To_WGS_1984_5')
+            print('projecting {} into {}'.format(layer, folder))
+            arcpy.management.Project(join(SHARE, layer), join(folder, layer), arcpy.SpatialReference(3857), 'NAD_1983_To_WGS_1984_5')
 
 
 def add_layers_to_mxd(new_layers):
@@ -40,8 +47,8 @@ def add_layers_to_mxd(new_layers):
     for layer_name in new_layers:
         print(layer_name)
         layer = arcpy.mapping.Layer(template_path)
-        layer.name = layer_name.split('.')[-1]
-        layer.replaceDataSource(join(local_folder, 'raster.gdb'), 'FILEGDB_WORKSPACE', layer_name.split('.')[-1])
+        layer.name = basename(layer_name)
+        layer.replaceDataSource(join(local_folder, dirname(layer_name)), 'FILEGDB_WORKSPACE', layer.name)
         arcpy.mapping.AddLayer(mxd.activeDataFrame, layer, 'BOTTOM')
 
     mxd.save()
@@ -56,18 +63,19 @@ def add_layers_to_mxd(new_layers):
         print('ERROR! Could not find layer: %s' % name)
 
     print('validating that all indexes in the extents feature classes (SDE) are added as layers in mxd')
-    for i in range(4):
-        cur = arcpy.SearchCursor(join(sgid, lyrs[i].name), "In_House = 'Yes' AND SHOW = 'Y'")
-        print('searching %s' % lyrs[i])
+    for extent_layer in [l for l in lyrs if l.name.endswith('_Extents')]:
+        cur = arcpy.SearchCursor(join(sgid, extent_layer.name), "In_House = 'Yes' AND SHOW = 'Y'")
+        print('searching %s' % extent_layer)
         for row in cur:
-            FindLayer(row.Tile_Index.split('.')[2])
+            FindLayer(row.Tile_Index)
 
 
 try:
     new_layers = argv[1].split(';')
 except:
-    new_layers = raw_input('New layer(s) separated by ";": ').split(';')
+    new_layers = raw_input('New layer(s) including gdb separated by ";" (e.g. DRGs.gdb\Vintage_24K): ').split(';')
 
+add_layers_to_pallet(new_layers)
 add_layers_to_gdbs(new_layers)
 add_layers_to_mxd(new_layers)
 
--- scripts/RasterPallet.py
+++ scripts/RasterPallet.py
@@ -18,6 +18,7 @@ import raster_secrets as secrets
 
 
 current_folder = dirname(realpath(__file__))
+layers_json_file = join(current_folder, 'layers.json')
 
 
 class RasterPallet(Pallet):
@@ -38,12 +39,18 @@ class RasterPallet(Pallet):
             "DRG_Extents"
         ], {'source_workspace': self.sgid, 'destination_workspace': self.indices})
 
-        self.log.info('adding crates for all data in the agrc/raster share gdbs')
+        self.log.info('adding crates for all all layers from layer.json')
+        with open(layers_json_file) as file:
+            layers = loads(file.read())
+
         arcpy.env.workspace = secrets.SHARE
         for source_gdb in arcpy.ListWorkspaces(workspace_type='FileGDB'):
             self.log.debug(source_gdb)
             arcpy.env.workspace = source_gdb
             destination_gdb = join(self.staging_rack, basename(source_gdb))
-            self.copy_data.append(destination_gdb)
 
-            self.add_crates(arcpy.ListFeatureClasses(), {'source_workspace': source_gdb, 'destination_workspace': destination_gdb})
+            crate_layers = [name for name in arcpy.ListFeatureClasses() if name in layers]
+
+            if len(crate_layers) > 0:
+                self.add_crates(crate_layers, {'source_workspace': source_gdb, 'destination_workspace': destination_gdb})
+                self.copy_data.append(destination_gdb)
