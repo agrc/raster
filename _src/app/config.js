6056f7f2daec07aac7dd28b23f706249dcbf0c9a
stdavis <stdavis@utah.gov>
2021-04-15 16:17:24 -0600

    Release v2.2.6

--- _src/app/config.js
+++ _src/app/config.js
@@ -12,7 +12,7 @@ define([
 ) {
     var config = {
         appName: 'Raster',
-        version: '2.2.5',
+        version: '2.2.6',
         wkid: 3857,
         urls: {
             mapService: '/ArcGIS/rest/services/Raster/MapServer',
