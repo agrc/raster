c53c75dd12c57fe3c5788d449f58b600c99d5f2b
stdavis <stdavis@utah.gov>
2024-10-03 10:19:14 -0600

    chore: add release please token

--- _src/app/config.js
+++ _src/app/config.js
@@ -12,7 +12,7 @@ define([
 ) {
     var config = {
         appName: 'Raster',
-        version: '0.0.0',
+        version: '0.0.0', // x-release-please-version
         wkid: 3857,
         urls: {
             mapService: 'https://mapserv.utah.gov/arcgis/rest/services/Raster/MapServer',
