713e406a84ea2efe46bb5a56098b963a87211a63
stdavis <stdavis@utah.gov>
2025-09-26 13:14:59 -0600

    chore: log app version number to console

--- src/main.tsx
+++ src/main.tsx
@@ -25,6 +25,8 @@ if (import.meta.env.VITE_FIREBASE_CONFIG) {
   firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 }
 
+console.log(`Raster app version: ${import.meta.env.PACKAGE_VERSION}`);
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <FirebaseAppProvider config={firebaseConfig}>
