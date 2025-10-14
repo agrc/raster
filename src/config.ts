4e3afd3c40d3773a83a52b0eb5e2dac3ce86255d
stdavis <stdavis@utah.gov>
2025-10-14 16:27:49 -0600

    style: LiDAR -> Lidar
    Trying to get closer to a standard

--- src/config.ts
+++ src/config.ts
@@ -39,7 +39,7 @@ const config: Config = {
   MAP_ELEMENT_ID: 'map',
   PRODUCT_TYPES: {
     aerialPhotography: 'Aerial Photography',
-    lidar: 'LiDAR DEMs',
+    lidar: 'Lidar DEMs',
     usgsDem: 'USGS DEMs',
     autoDem: 'Auto-Correlated DEMs',
     contours: 'Contours',
@@ -47,7 +47,7 @@ const config: Config = {
   },
   PRODUCT_TYPE_DESCRIPTIONS: {
     aerialPhotography: 'Digital aerial photography',
-    lidar: 'Digital elevation models derived from LiDAR',
+    lidar: 'Digital elevation models derived from Lidar',
     usgsDem: 'U.S. Geological Survey digital elevation models from The National Map',
     autoDem: 'Digital elevation models derived through an auto-correlation process from aerial photography',
     contours: 'Contour lines created at various intervals from Lidar and other sources',
