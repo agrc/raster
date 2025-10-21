de55301c30ca35dad646cf9dbd81a35b1d61ae81
stdavis <stdavis@utah.gov>
2025-10-21 15:47:55 -0600

    chore: add preview decorators to stories

---
+++ .storybook/decorators/Preview.tsx
@@ -0,0 +1,10 @@
+import type { ComponentType } from 'react';
+import { PreviewContext } from '../../src/contexts/PreviewProvider';
+
+export const PreviewDecorator = (Story: ComponentType) => (
+  <PreviewContext.Provider
+    value={{ previewId: 'NAIP 2024 | 4-Band (county mosaic)', removePreview: () => {}, addPreview: () => {} }}
+  >
+    <Story />
+  </PreviewContext.Provider>
+);
