e1a6f819d73139baba093e680e3ddf19566ed114
stdavis <stdavis@utah.gov>
2025-10-31 16:30:31 -0600

    feat: indicate which tiles have been downloaded
    Closes #247

--- src/contexts/TilesContextProvider.tsx
+++ src/contexts/TilesContextProvider.tsx
@@ -1,12 +1,29 @@
-import { createContext, useState } from 'react';
+import { createContext, useCallback, useState } from 'react';
 
 export const TilesContext = createContext<{
   count: number | null;
   setCount: (count: number | null) => void;
+  downloadedTiles: Set<number>;
+  markAsDownloaded: (objectId: number) => void;
+  clear: () => void;
 } | null>(null);
 
 export default function TilesContextProvider({ children }: { children: React.ReactNode }) {
   const [count, setCount] = useState<number | null>(null);
+  const [downloadedTiles, setDownloadedTiles] = useState<Set<number>>(new Set());
 
-  return <TilesContext.Provider value={{ count, setCount }}>{children}</TilesContext.Provider>;
+  const markAsDownloaded = useCallback((objectId: number) => {
+    setDownloadedTiles((prev) => new Set(prev).add(objectId));
+  }, []);
+
+  const clear = useCallback(() => {
+    setCount(null);
+    setDownloadedTiles(new Set());
+  }, []);
+
+  return (
+    <TilesContext.Provider value={{ count, setCount, downloadedTiles, markAsDownloaded, clear }}>
+      {children}
+    </TilesContext.Provider>
+  );
 }
